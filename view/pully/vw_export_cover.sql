dfcc04ccc07f2a763368ea84704a8eeca5c4e54b
ponceta <arnaudponcetmontanges@gmail.com>
2019-12-09 17:38:26 +0100

    Fix export view for cover shapes

--- view/pully/vw_export_cover.sql
+++ view/pully/vw_export_cover.sql
@@ -2,8 +2,10 @@ CREATE OR REPLACE VIEW qgep_sigip.vw_export_cover AS
   SELECT cover.obj_id,
     cover.identifier AS identification,
     material.value_fr AS materiau,
+    shape.value_fr AS forme,
     cover.level AS altitude,
     cover.situation_geometry AS the_geom
   FROM qgep_od.vw_cover cover
     LEFT JOIN qgep_vl.cover_material material ON material.code = cover.material
+    LEFT JOIN qgep_vl.cover_cover_shape shape ON shape.code = cover.cover_shape
 ;
