5cad55e63a5bf62ff8d4e274e5789bc7398a952e
ponceta <arnaudponcetmontanges@gmail.com>
2020-03-30 17:09:12 +0200

    Rectifie les liens vers windows pour Crystal report

--- view/pully/vw_export_cr_photo_min.sql
+++ view/pully/vw_export_cr_photo_min.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE VIEW qgep_sigip.vw_export_cr_photo_min AS
   SELECT ws.obj_id,
-  min(file._url) AS url,
+  regexp_replace(min(file._url), '\/'::text, '\'::text, 'g'::text) AS url,
   count(*) AS nbre_photos
   FROM qgep_od.vw_qgep_wastewater_structure ws
   LEFT JOIN qgep_od.vw_file file ON ws.obj_id = file.object
